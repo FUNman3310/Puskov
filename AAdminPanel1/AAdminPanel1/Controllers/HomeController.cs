bc25c27357573b7c41d57995fb9d19b74f1638db
FUNman3310 <tu7ldh0l3@code.edu.az>
2023-01-09 05:40:33 +0400

    halfdone

---
+++ AAdminPanel1/AAdminPanel1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using AAdminPanel1.Models;
+using AAdminPanel1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AAdminPanel1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AdminContext _adminContext;
+
+        public HomeController(AdminContext adminContext)
+        {
+            _adminContext = adminContext;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewModel homeVM = new HomeViewModel()
+            {
+                //Sliders = _adminContext.Sliders..OrderBy(x => x.Order).ToList()
+            };
+            return View(homeVM);
+        }
+
+       
+    }
+}
